2165b52fece8e736e932182565dcbdb000cb9886
David Barrios <davidbarrios@maseni.com>
2017-02-20 20:43:53 -0600

    Close the loop copy from @thegreatjerboa's changes that weren't pushed

--- Steamworks/src/org/usfirst/frc/team2508/robot/subsystems/DriveSystem.java
+++ Steamworks/src/org/usfirst/frc/team2508/robot/subsystems/DriveSystem.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2508.robot.subsystems;
 
+import org.usfirst.frc.team2508.robot.RobotMap;
+
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.FeedbackDevice;
 import com.ctre.CANTalon.TalonControlMode;
@@ -22,7 +24,14 @@ public class DriveSystem extends Subsystem {
 		lowerLeftTalon = new CANTalon(1);
 		upperRightTalon = new CANTalon(3);
 		lowerRightTalon = new CANTalon(4);
-
+		
+		initTalonSet(upperLeftTalon, lowerLeftTalon);
+		initTalonSet(upperRightTalon, lowerRightTalon);
+		
+		upperRightTalon.reverseOutput(true);
+		upperLeftTalon.reverseSensor(true);
+		upperLeftTalon.reverseOutput(false);
+		upperRightTalon.reverseSensor(false);
 	}
 	
 	public void initDefaultCommand() {
@@ -30,22 +39,38 @@ public class DriveSystem extends Subsystem {
 		//setDefaultCommand(new MySpecialCommand());
 		
 	}
-	public void initTalonSet(CANTalon mainTalon, CANTalon followerTalon, int mainProfile, int CANSpeedID){
-		mainTalon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-		mainTalon.reverseSensor(false);
+	public void initTalonSet(CANTalon mainTalon, CANTalon followerTalon){
+		mainTalon.setFeedbackDevice(FeedbackDevice.QuadEncoder);
 		mainTalon.configNominalOutputVoltage(+0.0f, -0.0f);
 		mainTalon.configPeakOutputVoltage(+12.0f, -12.0f);
-		mainTalon.setProfile(CANSpeedID);
-		//mainTalon.setF(0.1097);
-		mainTalon.setP(.8);
-		mainTalon.setI(.01); 
+		mainTalon.changeControlMode(TalonControlMode.Speed);
+		mainTalon.setProfile(0);
+		mainTalon.set(0);
+		
+		mainTalon.setP(0.5);
+		mainTalon.setI(0.001); 
 		mainTalon.setD(0);
+		mainTalon.setF(0);
+		mainTalon.setCloseLoopRampRate(0.5);
     	
 		followerTalon.changeControlMode(TalonControlMode.Follower);
-		followerTalon.set(mainProfile);		
+		followerTalon.set(mainTalon.getDeviceID());		
 	}
 	public void drive(double powerL, double powerR){
-		upperLeftTalon.set(powerL);
-		upperRightTalon.set(-powerR);
+		upperLeftTalon.set(processDeadband(powerL));
+		upperRightTalon.set(processDeadband(powerR));
+	}
+	
+	private double processDeadband(double input){
+		double output;
+		
+		if(Math.abs(input)< RobotMap.DeadbandThreshold){
+			output = 0;
+		}
+		
+		else {
+			output = input * RobotMap.MaxDriveRPM;
+		}
+		return output;
 	}
 }
